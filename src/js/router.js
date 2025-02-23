6d98c269058eb76b35fdc520c476238943939e29
KisekiMiracle <luisazaldegui99@gmail.com>
2025-02-23 04:56:03 -0500

    added some styles

--- src/js/router.js
+++ src/js/router.js
@@ -2,7 +2,7 @@ const urlPageTitle = "Arcadia Project";
 
 document.addEventListener("click", (e) => {
   const { target } = e;
-  if (!target.matches("nav a")) {
+  if (!target.matches(".page-header nav a")) {
     return;
   }
   e.preventDefault();
@@ -11,17 +11,17 @@ document.addEventListener("click", (e) => {
 
 const routes = {
   404: {
-    template: "./pages/404.html",
+    template: "/pages/404.html",
     title: `404 | ${urlPageTitle}`,
     description: "Page not found.",
   },
   "/": {
-    template: "./pages/home.html",
+    template: "/pages/home.html",
     title: `Home | ${urlPageTitle}`,
     description: "The Home page.",
   },
   "/characters": {
-    template: "./pages/characters.html",
+    template: "/pages/characters.html",
     title: `Characters | ${urlPageTitle}`,
     description: "The Characters page.",
   },
@@ -34,10 +34,21 @@ const router = (event) => {
   urlLocationHandler();
 };
 
+const handleHeaderActivePage = (location) => {
+  switch (location) {
+    case "/":
+      document.querySelectorAll("nav a")[0].classList.add("c-header-item-active");
+      break;
+    case "/characters":
+      document.querySelectorAll("nav a")[1].classList.add("c-header-item-active");
+      break;
+  }
+};
+
 const urlLocationHandler = async () => {
   const location = window.location.pathname;
   if (location.length == 0) {
-    location = "./";
+    location = "/";
   }
 
   const route = routes[location] || routes[404];
@@ -46,6 +57,7 @@ const urlLocationHandler = async () => {
   document.querySelector("#app").innerHTML = html;
   document.title = route.title;
   document.querySelector('meta[name="description"]').setAttribute("content", route.description);
+  handleHeaderActivePage(location);
 };
 
 window.onpopstate = urlLocationHandler;
--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
+        404: resolve(__dirname, "pages/404.html"),
         index: resolve(__dirname, "index.html"),
         home: resolve(__dirname, "pages/home.html"),
         characters: resolve(__dirname, "pages/characters.html"),
